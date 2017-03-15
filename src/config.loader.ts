c1dba975275fd2b1aaafb6327e365e0f58c846e4
zjx20 <zhoujianxiong2@gmail.com>
2017-03-15 16:28:33 +0800

    feat: introduced ConfigMergedLoader

--- src/config.loader.ts
+++ src/config.loader.ts
@@ -3,7 +3,11 @@ import { Http } from '@angular/http';
 
 // libs
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/reduce';
+import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/toPromise';
+import { Observable } from 'rxjs';
+import { mergeDeep } from 'typescript-object-utils';
 
 export abstract class ConfigLoader {
   abstract loadSettings(): any;
@@ -29,5 +33,26 @@ export class ConfigHttpLoader implements ConfigLoader {
       .toPromise()
       .then((settings: any) => settings)
       .catch(() => Promise.reject('Endpoint unreachable!'));
+  }
+}
+
+export class ConfigMergedLoader implements ConfigLoader {
+  constructor(private readonly http: Http,
+              private readonly paths: string[] = ['/config.json', '/config.local.json']) {
+  }
+
+  loadSettings(): any {
+    const errorIfEmpty: (source: Observable<any>) => Observable<any> = (source: Observable<any>) => {
+      return source.isEmpty()
+        .mergeMap((empty: boolean) => (empty) ? Observable.throw(new Error('empty')) : Observable.empty());
+    };
+    const jsons = Observable.onErrorResumeNext(this.paths.map(path => this.http.get(path)))
+      .map((res: any) => res.json())
+      .filter((x: any) => x !== null);
+    return jsons.merge(errorIfEmpty(jsons))
+      .reduce((x: any, y: any) => mergeDeep(x, y), {})
+      .toPromise()
+      .then((settings: any) => settings)
+      .catch(() => Promise.reject('Config was not loaded!'));
   }
 }
