ffe634fc3a7753ec572484264939576a56bdf862
Burak Tasci <burak.tasci@cozumtech.net>
2017-03-15 12:28:57 +0300

    build: fixed deps, gulp tasks, ignorers

--- config/gulp-tasks.js
+++ config/gulp-tasks.js
@@ -82,7 +82,9 @@ const ts = {
     return gulp.src([
       './index.ts',
       './src/**/*.ts',
-      '!./src/**/*.d.ts'
+      '!./src/**/*.d.ts',
+      './tests/**/*.ts',
+      '!./tests/**/*.d.ts'
     ])
       .pipe($.tslint({formatter: 'verbose'}))
       .pipe($.tslint.report({emitError: false}))
@@ -171,9 +173,9 @@ tasks.bundle = bundle;
 tasks.tests = tests;
 
 /**
- * Task: __CLEAN__
+ * Task: clean
  */
-gulp.task('__CLEAN__',
+gulp.task('clean',
   gulp.parallel(
     clean.temp,
     clean.bundles,
@@ -190,23 +192,23 @@ gulp.task('__CLEAN__',
  */
 gulp.task('make',
   gulp.series(
-    '__CLEAN__',
+    'clean',
     tasks.ts.compile,
     tasks.bundle.webpack
   ));
 
 /**
- * Task: _TEST_
+ * Task: test
  */
-gulp.task('_TEST_',
+gulp.task('test',
   gulp.series(
     tasks.tests.run
   ));
 
 /**
- * Task: review:ts
+ * Task: review:tslint
  */
-gulp.task('review:ts',
+gulp.task('review:tslint',
   gulp.series(
     tasks.ts.lint
   ));
