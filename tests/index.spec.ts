cac030d7c9af70674a1646c2eba0ba7f534bbe1a
Burak Tasci <burak.tasci@cozumtech.net>
2017-04-10 17:42:33 +0300

    test: refactor

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -7,6 +7,8 @@ import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@ang
 // module
 import { ConfigModule } from '../index';
 
+export const testPaths = ['/config.json', '/config.local.json', '/env/test.json'];
+
 export const testSettings = {
   'system': {
     'applicationName': 'Mighty Mouse',
@@ -14,6 +16,44 @@ export const testSettings = {
   },
   'i18n': {
     'locale': 'en'
+  }
+};
+
+export const testResponse = {
+  '/api/get-settings': testSettings,
+  '/config.json': {
+    'setting1': 'value1',
+    'setting2': 'from config.json',
+    'arr': [1, 2, 3],
+    'nested': {
+      'k1': 'v1',
+      'k2': 'v2 from config.json'
+    }
+  },
+  '/config.local.json': {
+    'setting2': 'from config.local.json',
+    'setting3': 'value3',
+    'arr': [4, 5]
+  },
+  '/env/test.json': {
+    'setting4': 'value4',
+    'nested': {
+      'k2': 'v2 from env/test.json',
+      'k3': 'v3'
+    }
+  }
+};
+
+export const testMergedSettings = {
+  'setting1': 'value1',
+  'setting2': 'from config.local.json',
+  'setting3': 'value3',
+  'setting4': 'value4',
+  'arr': [4, 5],
+  'nested': {
+    'k1': 'v1',
+    'k2': 'v2 from env/test.json',
+    'k3': 'v3'
   }
 };
 
