fbaec64d5859b2358a29ec4d97b43cff8f8c8e3f
Burak Tasci <burak.tasci@cozumtech.net>
2017-04-22 17:42:43 +0300

    test: make get method can retrieve value at any deep (#12)

--- tests/config.service.spec.ts
+++ tests/config.service.spec.ts
@@ -32,10 +32,12 @@ describe('@ngx-config/core:',
               config.loader.loadSettings()
                 .then(() => {
                   expect(config.getSettings()).toEqual(testSettings);
+                  expect(config.getSettings('')).toEqual(testSettings);
+                  expect(config.getSettings([])).toEqual(testSettings);
                 });
             }));
 
-        it('should be able to get settings using `group`',
+        it('should be able to get setting(s) using `key`',
           inject([ConfigService],
             (config: ConfigService) => {
               config.loader.loadSettings()
@@ -45,44 +47,39 @@ describe('@ngx-config/core:',
                       'applicationName': 'Mighty Mouse',
                       'applicationUrl': 'http://localhost:8000'
                     });
-                  expect(config.getSettings('system').applicationName).toEqual('Mighty Mouse');
-                  expect(config.getSettings('system').applicationUrl).toEqual('http://localhost:8000');
+
+                  expect(config.getSettings(['system', 'applicationName'])).toEqual('Mighty Mouse');
+                  expect(config.getSettings('system.applicationName')).toEqual('Mighty Mouse');
+
+                  expect(config.getSettings(['system', 'applicationUrl'])).toEqual('http://localhost:8000');
+                  expect(config.getSettings('system.applicationUrl')).toEqual('http://localhost:8000');
+
                   expect(config.getSettings('i18n'))
                     .toEqual({
                       'locale': 'en'
                     });
-                  expect(config.getSettings('i18n').locale).toEqual('en');
+
+                  expect(config.getSettings(['i18n', 'locale'])).toEqual('en');
+                  expect(config.getSettings('i18n.locale')).toEqual('en');
                 });
             }));
 
-        it('should throw if you provide an invalid `group`',
+        it('should be able to get `default value` w/invalid `key`',
+          inject([ConfigService],
+            (config: ConfigService) => {
+              config.loader.loadSettings()
+                .then(() => {
+                  expect(config.getSettings('layout', 'default')).toEqual('default');
+                });
+            }));
+
+        it('should throw if you provide an invalid `key` w/o `default value`',
           inject([ConfigService],
             (config: ConfigService) => {
               config.loader.loadSettings()
                 .then(() => {
                   expect(() => config.getSettings('layout'))
-                    .toThrowError('No setting found with the specified group [layout]!');
-                });
-            }));
-
-        it('should be able to get settings using `group`/`key` combination',
-          inject([ConfigService],
-            (config: ConfigService) => {
-              config.loader.loadSettings()
-                .then(() => {
-                  expect(config.getSettings('system', 'applicationName')).toEqual('Mighty Mouse');
-                  expect(config.getSettings('system', 'applicationUrl')).toEqual('http://localhost:8000');
-                  expect(config.getSettings('i18n', 'locale')).toEqual('en');
-                });
-            }));
-
-        it('should throw if you provide an invalid `key`',
-          inject([ConfigService],
-            (config: ConfigService) => {
-              config.loader.loadSettings()
-                .then(() => {
-                  expect(() => config.getSettings('system', 'workingTheme'))
-                    .toThrowError('No setting found with the specified group/key [system/workingTheme]!');
+                    .toThrowError('No setting found with the specified key [layout]!');
                 });
             }));
       });
