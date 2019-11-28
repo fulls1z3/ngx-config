169ff1043d5d8634838b366c45724026fbc00d0c
Michele Stieven <usergalileo@gmail.com>
2019-11-29 00:41:12 +0100

    Provide a forChild() method
    This change makes ConfigModule importable in lazy loaded modules with forChild() but also preserves the forRoot() check.

--- packages/@ngx-config/core/src/config.module.ts
+++ packages/@ngx-config/core/src/config.module.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, ModuleWithProviders, NgModule } from '@angular/core';
+import { APP_INITIALIZER, Inject, InjectionToken, ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 
 import { ConfigLoader, ConfigStaticLoader } from './config.loader';
 import { ConfigPipe } from './config.pipe';
@@ -13,17 +13,37 @@ export const initializerFactory = (config: ConfigService) => {
   return res;
 };
 
+export const CONFIG_FORROOT_GUARD = new InjectionToken<void>('CONFIG_FORROOT_GUARD');
+
+/**
+ * Used to check if the user has called ConfigModule.forRoot() in a lazy-loaded module.
+ */
+export function provideForRootGuard(config: ConfigService): any {
+  if (config) {
+    throw new Error(
+        `ConfigModule.forRoot() called twice. Lazy loaded modules should use ConfigModule.forChild() instead.`);
+  }
+  return 'guarded';
+}
+
 @NgModule({
   declarations: [ConfigPipe],
   exports: [ConfigPipe]
 })
 export class ConfigModule {
+  
+  /**
+   * By injecting the guard we're making sure that the check will be made every time
+   * a new ConfigModule is imported, both in the root injector and in lazy loaded modules.
+   */
+  constructor(@Optional() @Inject(CONFIG_FORROOT_GUARD) guard: any) {}
+  
   static forRoot(
     configuredProvider: any = {
       provide: ConfigLoader,
       useFactory: configFactory
     }
-  ): ModuleWithProviders {
+  ): ModuleWithProviders<ConfigModule> {
     return {
       ngModule: ConfigModule,
       providers: [
@@ -34,8 +54,19 @@ export class ConfigModule {
           useFactory: initializerFactory,
           deps: [ConfigService],
           multi: true
+        },
+        {
+          provide: CONFIG_FORROOT_GUARD,
+          useFactory: provideForRootGuard,
+          deps: [[ConfigService, new Optional(), new SkipSelf()]]
         }
       ]
+    };
+  }
+  
+  static forChild(): ModuleWithProviders<ConfigModule> {
+    return {
+      ngModule: ConfigModule
     };
   }
 }
